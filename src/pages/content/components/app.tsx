e7fb71be9b53f52d798e825ae2e3c91400bed6e4
KGESH <seonghyeon.jee@gmail.com>
2023-06-22 23:38:36 +0900

    [Style] 커서 위치 모달

--- src/pages/content/components/app.tsx
+++ src/pages/content/components/app.tsx
@@ -1,8 +1,9 @@
 import { useCapture } from "@pages/content/hooks/useCapture";
 import { useEffect, useState } from "react";
-import { ResultModal } from "@pages/content/components/modal/ResultModal";
+import { MyModal, ResultModal } from "@pages/content/components/modal/ResultModal";
 import { CursorPos, useCursor } from "@pages/content/hooks/useCursor";
 import { DragArea } from "@pages/content/components/cursor/DragArea";
+import { ChakraProvider, useDisclosure } from "@chakra-ui/react";
 
 export const useBlockSelectText = (isDragging: boolean) => {
   const [isSelectTextBlocking, setIsSelectTextBlocking] = useState(false);
@@ -21,29 +22,33 @@ export const useBlockSelectText = (isDragging: boolean) => {
 };
 
 export default function App() {
-  const { isDragging, draggedArea, isCopied } = useCapture();
+  const { isDragging, draggedArea } = useCapture();
   const { isSelectTextBlocking } = useBlockSelectText(isDragging);
-  const [modalVisible, setModalVisible] = useState(false);
   const [modalPos, setModalPos] = useState<CursorPos>({ x: 0, y: 0 });
   const { cursorPos } = useCursor();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const showModal = () => {
-    setModalVisible(!modalVisible);
-    if (!modalVisible) setModalPos(cursorPos);
+    if (!isOpen) {
+      setModalPos(cursorPos);
+      onOpen();
+    }
+
+    // setModalVisible(!modalVisible);
+    // if (!modalVisible) setModalPos(cursorPos);
   };
 
   return (
-    <div className="content-view">
-      <button onClick={showModal}>Show Modal</button>
+    <ChakraProvider>
+      <div className="content-view">
+        <button onClick={showModal}>Show Modal</button>
 
-      {/** Show dragging area */}
-      {isDragging && <DragArea {...draggedArea} />}
+        {/** Show dragging area */}
+        {isDragging && <DragArea {...draggedArea} />}
 
-      {modalVisible && (
-        <ResultModal isCopied={isCopied} {...modalPos}>
-          <p>{`hello world!`}</p>
-        </ResultModal>
-      )}
-    </div>
+        {/*{modalVisible && <ResultModal text={`hello`} {...modalPos}></ResultModal>}*/}
+        {<MyModal isOpen={isOpen} onClose={onClose} text={`hello`} {...modalPos}></MyModal>}
+      </div>
+    </ChakraProvider>
   );
 }
